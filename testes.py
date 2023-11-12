1c665408e619838d0fba53d72403506a6f2dcc14
Gabriel Monteiro <gabrieloliveiramonteiro@hotmail.com>
2023-11-12 17:30:31 -0300

    Update testes.py

--- testes.py
+++ testes.py
@@ -1,38 +1,7 @@
 from classes import Usuario, Produtos, Clientes, Vendas
 import unittest
 import os
-
-class TestUsuario(unittest.TestCase):
-
-  def setUp(self):
-    # Inicializa o objeto Usuario para os testes
-    self.usuario = Usuario()
-
-  def test_criar_tabela(self):
-    # Testa se criar uma tabela de usuários resulta na criação do arquivo
-    self.usuario.criar_tabela()
-    self.assertTrue(os.path.exists(self.usuario.arquivo_login))
-
-  def test_usuario_existe(self):
-    # Testa se o método identifica corretamente usuários existentes
-    self.assertFalse(self.usuario.usuario_existe('test_user'))
-    self.usuario.cadastrar_usuario('test_user', 'test_password')
-    self.assertTrue(self.usuario.usuario_existe('test_user'))
-
-  def test_cadastrar_usuario(self):
-    # Testa se um novo usuário é adicionado com sucesso à tabela
-    self.usuario.cadastrar_usuario('test_user', 'test_password')
-    self.assertTrue(self.usuario.usuario_existe('test_user'))
-
-  def test_realizar_login(self):
-    # Testa se a função de login autentica com sucesso um usuário
-    self.usuario.cadastrar_usuario('test_user', 'test_password')
-    self.assertTrue(self.usuario.realizar_login('test_user', 'test_password'))
-
-  def tearDown(self):
-    # Limpeza removendo o arquivo criado
-    os.remove(self.usuario.arquivo_login)
-
+import pandas as pd
 
 
 class TestProdutos(unittest.TestCase):
@@ -52,15 +21,6 @@ class TestProdutos(unittest.TestCase):
     df = pd.read_excel('produtos.xlsx')
     self.assertTrue('TestProduct' in df['Nome'].values)
 
-  def test_cadastrar_produto_tela(self):
-    # Escreva casos de teste para a interface gráfica do usuário
-    # Explicação: Testes de interface gráfica geralmente envolvem interações complexas
-    # e podem exigir ferramentas de teste especializadas, como unittest.mock.
-
-  def tearDown(self):
-    # Limpeza removendo o arquivo criado
-    os.remove('produtos.xlsx')
-
 
 class TestClientes(unittest.TestCase):
 
@@ -74,29 +34,20 @@ class TestClientes(unittest.TestCase):
     self.assertTrue(os.path.exists('clientes.xlsx'))
 
   def test_validar_nome_cliente(self):
-    # Testa se o método valida corretamente os nomes dos clientes
-    self.assertTrue(self.clientes.validar_nome_cliente('John Doe'))
+    # Testa a validação do nome do cliente para entrada válida
+    self.assertTrue(self.clientes.validar_nome_cliente('Lucas Oliveira'))
+
+  def test_validar_nome_cliente_invalido(self):
+    # Testa a validação do nome do cliente para entrada inválida (números)
     self.assertFalse(self.clientes.validar_nome_cliente('1234'))
 
   def test_validar_telefone(self):
-    # Testa se o método valida corretamente os números de telefone
+    # Testa a validação do telefone para entrada válida
     self.assertTrue(self.clientes.validar_telefone('(12) 34567-8901'))
-    self.assertFalse(self.clientes.validar_telefone('12345678901'))
 
-  def test_cadastrar_cliente(self):
-    # Testa se um novo cliente é adicionado com sucesso à tabela
-    self.clientes.cadastrar_cliente('TestClient', '(11) 98765-4321')
-    df = pd.read_excel('clientes.xlsx')
-    self.assertTrue('TestClient' in df['Nome'].values)
-
-  def test_cadastrar_cliente_tela(self):
-    # Escreva casos de teste para a interface gráfica do usuário
-    # Explicação: Testes de interface gráfica geralmente envolvem interações complexas
-    # e podem exigir ferramentas de teste especializadas, como unittest.mock.
-
-  def tearDown(self):
-    # Limpeza removendo o arquivo criado
-    os.remove('clientes.xlsx')
+  def test_validar_telefone_invalido(self):
+    # Testa a validação do telefone para entrada inválida
+    self.assertFalse(self.clientes.validar_telefone('123.456.789-10'))
 
 
 
@@ -123,15 +74,7 @@ class TestVendas(unittest.TestCase):
     df_produtos = pd.read_excel('produtos.xlsx')
     self.assertEqual(int(df_produtos[df_produtos['Nome'] == 'TestProduct']['Quantidade']), 5)
 
-  def test_cadastrar_venda_tela(self):
-    # Escreva casos de teste para a interface gráfica do usuário
-    # Explicação: Testes de interface gráfica geralmente envolvem interações complexas
-    # e podem exigir ferramentas de teste especializadas, como unittest.mock.
 
-  def tearDown(self):
-    # Limpeza removendo os arquivos criados
-    os.remove('vendas.xlsx')
-    os.remove('produtos.xlsx')
 
 
 
