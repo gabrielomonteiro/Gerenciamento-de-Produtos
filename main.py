1d9a0aa043e7520890a915387049acb625990a1c
Gabriel Monteiro <gabrieloliveiramonteiro@hotmail.com>
2023-11-12 17:30:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,4 @@
-from classes import Usuario, Produtos, Clientes, Vendas
-import PySimpleGUI as sg
+import pytest
+import os
 
-def main():
-  # Função principal do programa
-  Usuario().criar_tabela()
-  Produtos().criar_tabela()
-  Clientes().criar_tabela()
-  Vendas().criar_tabela()
-
-  layout_cadastro = [
-      [sg.Text('Nome de Usuário:'), sg.InputText(key='-USUARIO_CAD-')],
-      [sg.Text('Senha:'), sg.InputText(key='-SENHA_CAD-', password_char='*')],
-      [sg.Button('Cadastrar'), sg.Button('Cancelar')]
-  ]
-
-  layout_login = [
-      [sg.Text('Nome de Usuário:'), sg.InputText(key='-USUARIO_LOGIN-')],
-      [sg.Text('Senha:'), sg.InputText(key='-SENHA_LOGIN-', password_char='*')],
-      [sg.Button('Login', bind_return_key=True), sg.Button('Cancelar')]
-  ]
-
-  janela_login = sg.Window('Tela de Login', layout_login)
-
-  while True:
-      evento, valores = janela_login.read()
-
-      if evento == sg.WIN_CLOSED or evento == 'Cancelar':
-          break
-      elif evento == 'Cadastrar':
-          Usuario().cadastrar_usuario(valores['-USUARIO_CAD-'], valores['-SENHA_CAD-'])
-      elif evento == 'Login':
-          Usuario().realizar_login(valores['-USUARIO_LOGIN-'], valores['-SENHA_LOGIN-'])
-
-  janela_login.close()
-
-if __name__ == "__main__":
-  main()
+os.system('pytest -vv testes.py')
